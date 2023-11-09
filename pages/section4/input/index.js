a1c71bf9ececd002b0f91b8cfb0757063a1a9753
junogit01 <esens0607@gmail.com>
2023-11-09 21:26:57 +0900

    231109

--- pages/section4/input/index.js
+++ pages/section4/input/index.js
@@ -28,6 +28,7 @@ const CREATE_BOARD = gql`
   mutation createBoard($createBoardInput: CreateBoardInput!) {
     createBoard(createBoardInput: $createBoardInput) {
       _id
+      writer
     }
   }
 `;
