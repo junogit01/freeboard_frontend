9fb6f5f6bebf3aaadc73389fee6090392f592ed3
junogit01 <esens0607@gmail.com>
2023-11-11 01:02:22 +0900

    231111

--- styles/boardcss.js
+++ styles/boardcss.js
@@ -3,16 +3,81 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   width: 1200px;
   margin: 100px;
-  border: 1px solid lightgray;
 `;
 
-export const Button = styled.div`
+export const CardWrapper = styled.div`
+  border: 1px solid black;
+  padding-top: 80px;
+  padding-bottom: 100px;
+  padding-left: 102px;
+  padding-right: 102px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: none;
+  box-shadow: 0px 0px 10px gray;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #bdbdbd;
+  padding-bottom: 20px;
+`;
+
+export const AvatarWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Avatar = styled.img`
+  margin-right: 10px;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Writer = styled.div``;
+
+export const CreatedAt = styled.div``;
+
+export const Body = styled.div`
+  width: 100%;
+  min-height: 800px;
+`;
+
+export const Title = styled.h1`
+  padding-top: 80px;
+`;
+
+export const Contents = styled.div`
+  padding-top: 40px;
+  padding-bottom: 120px;
+`;
+
+export const BottomWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-top: 80px;
+`;
+
+export const Button = styled.button`
   width: 179px;
   height: 45px;
-  border: 1px solid lightgray;
-  padding: 14px 60px;
-  font-size: 16px;
-  font-weight: 500;
-  justify-contents: center;
-  align-items: center;
+  background-color: white;
+  border: 1px solid gray;
+  margin: 0px 12px;
+  cursor: pointer;
+
+  :hover {
+    background-color: gold;
+    border-color: white;
+  }
 `;
