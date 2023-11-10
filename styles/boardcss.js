215c2024c4aeb055bccf9b05c1b2853e5588899f
junogit01 <esens0607@gmail.com>
2023-11-11 00:40:45 +0900

    231111

---
+++ styles/boardcss.js
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  margin: 100px;
+  border: 1px solid lightgray;
+`;
+
+export const Button = styled.div`
+  width: 179px;
+  height: 45px;
+  border: 1px solid lightgray;
+  padding: 14px 60px;
+  font-size: 16px;
+  font-weight: 500;
+  justify-contents: center;
+  align-items: center;
+`;
