0f95d124b0552ee07ef8817d54ed9aeb6e20e6b0
junogit01 <esens0607@gmail.com>
2023-11-11 00:39:52 +0900

    231111

--- pages/board/index.js
+++ pages/board/index.js
@@ -1,5 +1,49 @@
-import { Wrapper } from "../../styles/boardcss";
+import { Wrapper, Button } from "../../styles/boardcss";
+import { useQuery, gql } from "@apollo/client";
+
+const FETCH_BOARD = gql`
+  query {
+    fetchBoard(number: 8) {
+      number
+      writer
+      createdAt
+      title
+      contents
+    }
+  }
+`;
 
 export default function BoardPage() {
-  return <Wrapper></Wrapper>;
+  const { data } = useQuery(FETCH_BOARD);
+  console.log(data);
+  return (
+    <Wrapper>
+      <div class="peoplesection">
+        <div class="profile"></div>
+        <div class="writer">{data?.fetchBoard?.writer}</div>
+        <div class="createdAt">{data?.fetchBoard?.createdAt}</div>
+      </div>
+      <div class="body">
+        <div class="bodyTitle">{data?.fetchBoard?.title}</div>
+        <img></img>
+        <div class="bodyContents">{data?.fetchBoard?.contents}</div>
+        <div class="bodyVideo"></div>
+      </div>
+      <div class="footer"></div>
+      <dlv class="btn">
+        <div class="listbtn">
+          <Button>목록으로</Button>
+          <Button>수정하기</Button>
+          <Button>삭제하기</Button>
+        </div>
+      </dlv>
+    </Wrapper>
+  );
 }
+
+// <div>
+//   <div>1번 페이지 이동이 완료되었습니다.</div>
+//   <div>작성자: {data?.fetchBoard.writer}</div>
+//   <div>제목: {data?.fetchBoard.title}</div>
+//   <div>내용: {data ? data.fetchBoard.contents : "로딩중입니다."}</div>
+// </div>
--- styles/boardcss.js
+++ styles/boardcss.js
@@ -2,16 +2,17 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   width: 1200px;
-  /* height: 1847px; */
-  border: 1px solid black;
   margin: 100px;
-  padding-top: 80px;
-  padding-bottom: 100px;
-  padding-left: 102px;
-  padding-right: 102px;
-  display: flex;
-  flex-direction: column;
+  border: 1px solid lightgray;
+`;
+
+export const Button = styled.div`
+  width: 179px;
+  height: 45px;
+  border: 1px solid lightgray;
+  padding: 14px 60px;
+  font-size: 16px;
+  font-weight: 500;
+  justify-contents: center;
   align-items: center;
-  border: none;
-  box-shadow: 0px 0px 10px gray;
 `;
