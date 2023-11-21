103447f004ba0e8a81ae6939051a8760af9a1194
junogit01 <esens0607@gmail.com>
2023-11-21 19:41:36 +0900

    231121

--- src/components/units/board/list/BoardList.presenter.js
+++ src/components/units/board/list/BoardList.presenter.js
@@ -4,24 +4,24 @@ import getDate from "./getDate";
 export default function BoardListUI(props) {
   return (
     <S.Wrapper class="wrapper">
-      <div class="table">
+      <S.Twrap class="table">
         <S.Table>
-          <th>번호</th>
-          <th>제목</th>
-          <th>작성자</th>
-          <th>날짜</th>
+          <S.THNumber>번호</S.THNumber>
+          <S.THTitle>제목</S.THTitle>
+          <S.THWriter>작성자</S.THWriter>
+          <S.ThCreatedAt>날짜</S.ThCreatedAt>
           {props.data?.fetchBoards.map((el) => (
             <tr key={el._id}>
-              <td>{String(el._id).slice(-4).toUpperCase()}</td>
-              <td id={el._id} onClick={props.onClickToBoardDetail}>
+              <S.TdNumber>{String(el._id).slice(-4).toUpperCase()}</S.TdNumber>
+              <S.TdTitle id={el._id} onClick={props.onClickToBoardDetail}>
                 {el.title}
-              </td>
-              <td>{el.writer}</td>
-              <td>{getDate(el.createdAt)}</td>
+              </S.TdTitle>
+              <S.TdWriter>{el.writer}</S.TdWriter>
+              <S.TdCreatedAt>{getDate(el.createdAt)}</S.TdCreatedAt>
             </tr>
           ))}
         </S.Table>
-      </div>
+      </S.Twrap>
       <S.Footer>
         <S.Button onClick={props.onClickToBoardNew}>
           <img src="/images/boards/list/write.png" alt="Pencil Img" />
--- src/components/units/board/list/BoardList.styles.js
+++ src/components/units/board/list/BoardList.styles.js
@@ -3,6 +3,13 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   width: 1200px;
   margin: 100px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Twrap = styled.div`
+  width: 100%;
+  margin-bottom: 25px;
 `;
 
 export const Table = styled.table`
@@ -30,4 +37,54 @@ export const Button = styled.button`
 export const Footer = styled.footer`
   display: flex;
   justify-content: right;
+  width: 75%;
+`;
+
+export const THNumber = styled.th`
+  width: 10%;
+  text-align: center;
+  border-bottom: 1px solid lightgray;
+  padding: 10px;
+`;
+
+export const THTitle = styled.th`
+  width: 70%;
+  text-align: center;
+  border-bottom: 1px solid lightgray;
+  padding: 10px;
+`;
+
+export const THWriter = styled.th`
+  width: 10%;
+  text-align: center;
+  border-bottom: 1px solid lightgray;
+  padding: 10px;
+`;
+
+export const ThCreatedAt = styled.th`
+  width: 10%;
+  text-align: center;
+  border-bottom: 1px solid lightgray;
+  padding: 10px;
+`;
+
+export const TdNumber = styled.td`
+  text-align: center;
+  padding: 10px;
+  border-bottom: 1px solid lightgray;
+`;
+
+export const TdTitle = styled.td`
+  text-align: center;
+  border-bottom: 1px solid lightgray;
+`;
+
+export const TdWriter = styled.td`
+  text-align: center;
+  border-bottom: 1px solid lightgray;
+`;
+
+export const TdCreatedAt = styled.td`
+  text-align: center;
+  border-bottom: 1px solid lightgray;
 `;
